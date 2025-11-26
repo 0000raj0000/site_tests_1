980e3c1475af13b1813d3dc684eca97e79ec0d70
0000raj0000 <rajnamadev0@gmail.com>
2025-11-26 14:25:39 +0530

    commit-1

---
+++ script.js
@@ -0,0 +1,28 @@
+var tl = gsap.timeline();
+
+tl.from(".ps",{
+    x:-500,
+    duration: 0.9,
+})
+tl.from(
+    ".click1",{
+        x:50,
+        y:50,
+        duration: 0.8,
+        opacity: 0
+    }
+)
+tl.to(
+    ".ps",{
+        scale:1.1,
+        duration: 0.2,
+        yoyo: true,
+    }
+)
+tl.from(
+    ".thirdli li",{
+        x:-200,
+        duration: 0.4,
+        stagger: 0.2
+    }
+)
